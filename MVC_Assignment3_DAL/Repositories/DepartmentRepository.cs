57a714ea88425f863b2b4344452466790a6efdbe
135ahmedsayed <ahmedshkra246@gmail.com>
2025-09-08 17:18:35 +0300

    CreateInterface(Properties) & Implemented it

--- MVC_Assignment3_DAL/Repositories/DepartmentRepository.cs
+++ MVC_Assignment3_DAL/Repositories/DepartmentRepository.cs
@@ -1,22 +1,52 @@
 ﻿using MVC_Assignment3_DAL.Context;
+using MVC_Assignment3_DAL.Entities;
 
 namespace MVC_Assignment3_DAL.Repositories;
-public class DepartmentRepository : IDepartmentRepository
+public class DepartmentRepository(CompanyDBContext dBContext) : IDepartmentRepository
 {
 
     #region Dependency Injection
     //Context Encapsulation
-    private CompanyDBContext _dBContext;
+    private CompanyDBContext _dBContext = dBContext;
     //Generate CTOR
-    public DepartmentRepository(CompanyDBContext dBContext)
+    //public DepartmentRepository(CompanyDBContext dBContext)
+    //{
+    //    _dBContext = dBContext;
+    //}
+
+    public int Add(Department department)
     {
-        _dBContext = dBContext;
+        _dBContext.Departments.Add(department);
+        return _dBContext.SaveChanges();
+    }
+
+    public int Delete(Department department)
+    {
+        _dBContext.Departments.Remove(department);
+        return _dBContext.SaveChanges();
+    }
+
+    public IEnumerable<Department> GetAll(bool trackChanges = false)
+    {
+        return trackChanges? _dBContext.Departments.ToList()
+            : _dBContext.Departments.AsNoTracking().ToList();
+    }
+
+    public Department? GetById(int id)
+    {
+        return _dBContext.Departments.Find(id);
+    }
+
+    public Department? GetByName(string name)
+    {
+        return _dBContext.Departments.FirstOrDefault(n => n.Name == name);
+    }
+
+    public int Update(Department department)
+    {
+        _dBContext.Departments.Update(department);
+        return _dBContext.SaveChanges();
     }
     #endregion
 
-    // Get 
-    // Get ALL
-    // Add
-    // Delete
-    // Update
 }
--- MVC_Assignment3_DAL/Repositories/IDepartmentRepository.cs
+++ MVC_Assignment3_DAL/Repositories/IDepartmentRepository.cs
@@ -1,4 +1,11 @@
 ﻿namespace MVC_Assignment3_DAL.Repositories;
 public interface IDepartmentRepository
 {
+    IEnumerable<Department> GetAll(bool trackChanges = false);
+    Department? GetById(int id);
+    Department? GetByName(string name);
+    int Add(Department department);
+    int Update(Department department);
+    int Delete(Department department);
+
 }
